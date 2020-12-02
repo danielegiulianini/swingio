f2ca75476a73c104bee677d4d39d112f8de46773
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-02 17:19:19 +0100

    add MonadicActionListener type alias

--- src/main/scala/swingio/scala/package.scala
+++ src/main/scala/swingio/scala/package.scala
@@ -1,7 +1,12 @@
 package swingio
 
+import java.awt.event.ActionEvent
+
 import cats.effect.IO
 
 package object scala {
   implicit def fromGenericTypeToIO[T](fa : => T) : IO[T] = IO{ fa }
+
+  type MonadicActionListener = ActionEvent => IO[Unit]
+  
 }
