a5970e7a3f9622f713b86551163f3c1951d84bbf
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-04 00:13:40 +0100

    add monadicInvokeLater

--- src/main/scala/swingio/scala/ThreadingUtilities.scala
+++ src/main/scala/swingio/scala/ThreadingUtilities.scala
@@ -1,9 +1,20 @@
 package swingio.scala
 
 import cats.effect.IO
+import javax.swing.SwingUtilities
 
 object ThreadingUtilities {
   implicit def fromByNameUnitToRunnable(fa : => Unit): Runnable = () => fa
 
-  implicit def fromMonadicByNameUnitToRunnable(fa : => IO[Unit]): Runnable = () => fa.unsafeRunSync()
+  //implicit def fromMonadicByNameUnitToRunnable(fa : => IO[Unit]): Runnable = () => fa.unsafeRunSync()
+  object Monadic {
+    /*type MonadicRunnable = () => IO[Unit]
+    def invokeAndWait(monadicRunnable: MonadicRunnable) =
+      SwingUtilities.invokeAndWait(() => monadicRunnable.unsafeRunSync())*/
+
+
+    def monadicInvokeLater(byNameMonadicRunnable: =>IO[Unit]): Unit =
+      SwingUtilities.invokeLater(() => byNameMonadicRunnable.unsafeRunSync())
+     
+  }
 }
