c1fb169f79993e0206644ff257e947f322cb62af
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 15:35:35 +0100

    add methods with IO return type for adding into a Container

--- src/main/scala/swingio/scala/components/Container.scala
+++ src/main/scala/swingio/scala/components/Container.scala
@@ -1,10 +1,30 @@
 package swingio.scala.components
 
-import java.awt.Container
+import java.awt.{Component, Container}
+
+import cats.effect.IO
 
 object Container {
   implicit class ContainerIO(container: Container){
 
-   
+    /** Returns an [[IO]] containing the code for adding the [[Component]] wrapped in the given [[ComponentIO]] to
+     * the [[Container]] wrapped in this instance. It is the monadic counterpart of [[Container#added]].*/
+    def add(componentToBeAdded: Component): IO[Component] = IO {container.add(componentToBeAdded)}
+
+    /** Returns an [[IO]] containing the description of a [[java.awt.Container#add]]
+     * method invocation. It is the monadic counterpart of [[Container#added]].
+     * After adding the specified component at the end of this container, it also notifies
+     * the layout manager to add the component to this container's layout using the
+     * specified constraints object. */
+    def add(componentToBeAdded: Component, constraints: Object): IO[Unit] =
+      IO {container.add(componentToBeAdded, constraints)}
+
+
+    /** Returns an [[IO]] containing the description of a [[java.awt.Container#remove]]
+     * method invocation.*/
+    def removed(componentToBeAdded: Component): IO[Unit] =
+      IO { container.remove(componentToBeAdded)  }
+
+    
   }
 }
