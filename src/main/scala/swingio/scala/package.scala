19b208ca94a1fe821a475baa9e5c0083c4c184d8
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 17:05:59 +0100

    update scaladoc for package object

--- src/main/scala/swingio/scala/package.scala
+++ src/main/scala/swingio/scala/package.scala
@@ -8,15 +8,15 @@ package object scala extends listeners.ListenersAliases with listeners.Listeners
   with listeners.ComponentsWithListenersImplicits with concurrency.ConcurrencyImplicits
   with concurrency.ConcurrencyUtilities {
 
-  /** Converts from a value of generic type T to an IO containing that value, enabling use and
+  /** Converts from a value of generic subtype of java.awt.Component to an IO containing that value, enabling use and
    * composition inside a for-comprehension construct with the [[IO]] semantics, that is: sequential computation
    * blocks described by an IO and composed by flatmap and map results in sequential side-effect execution when run
    * by [[cats.effect.IO#unsafeRunSync]].
-   * This conversion allows to automatically convert any member of
-   * [[java.awt.Component]] to IO, actually getting it ready to be used in for-comprehesion, and to reuse the
-   * constructors provided by the original library without need to rewrite each of them and manually encapsulates
-   * their results into IO monads.
-   * 
+   * This conversion allows to automatically convert any member of [[java.awt.Component]] to IO, actually
+   * getting it ready to be used in for-comprehension, and to reuse the constructors provided by the original
+   * library without need to manually rewrite each of them in order to put it results into IO for enabling
+   * composition in the monadic chain.
+   *
    * @see [[swingio.scala.examples.ExampleOfUse]] for example of how to compose IOs, and
    * [[IO]] for a detailed description of the IO data type.*/
   implicit def fromGenericTypeToIO[T<:Component](fa : T) : IO[T] = IO{ fa }
