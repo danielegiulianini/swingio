5da89213f925a8c87beba639331d5b8172a3a6b2
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 23:48:24 +0100

    add implicit conversion for promise to IO

--- src/main/scala/swingio/scala/concurrency/ConcurrencyImplicits.scala
+++ src/main/scala/swingio/scala/concurrency/ConcurrencyImplicits.scala
@@ -1,5 +1,10 @@
 package swingio.scala.concurrency
 
+import cats.effect.IO
+
+import scala.concurrent.Promise
+
 trait ConcurrencyImplicits {
   implicit def fromByNameUnitToRunnable(fa : => Unit): Runnable = () => fa
+  implicit def fromPromiseToIOOfPromise[T](promise: Promise[T]) : IO[Promise[T]] = IO{promise}
 }
