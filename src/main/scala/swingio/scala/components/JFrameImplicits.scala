c0ae10ce94033e6180b107e84204494e84d6e6e0
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 15:57:00 +0100

    add implicit class for enabling IO return type for JFrame

--- src/main/scala/swingio/scala/components/JFrameImplicits.scala
+++ src/main/scala/swingio/scala/components/JFrameImplicits.scala
@@ -14,53 +14,7 @@ object JFrameImplicits {
      * method invocation. */
     def contentPane(): IO[Container] = IO { jFrame.getContentPane() }
 
-    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setResizable]]
-     * method invocation. */
-    def resizableSet(resizable: Boolean): IO[Unit] = IO {
-      component.setResizable(resizable)
-    }
 
-    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setVisible]]
-     * method invocation. */
-    def visibleSet(b: Boolean): IO[Unit] = IO {
-      component.setVisible(b)
-    }
-
-    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#pack]]
-     * method invocation. */
-    def packed(): IO[Unit] = IO {
-      component.pack()
-    }
-
-    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setSize]]
-     * method invocation. */
-    def sizeSet(width: Int, height: Int): IO[Unit] = IO {
-      component.setSize(width, height)
-    }
-
-    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setLocationRelativeTo]]
-     * method invocation. */
-    def locationRelativeToSet(c: Component): IO[Unit] = IO {
-      component.setLocationRelativeTo(c)
-    }
-
-    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setDefaultCloseOperation]]
-     * method invocation. */
-    def defaultCloseOperationSet(operation: Int): IO[Unit] = IO {
-      component.setDefaultCloseOperation(operation)
-    }
-
-    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setTitle]]
-     * method invocation. */
-    def titleSet(title: String): IO[Unit] = IO {
-      component.setTitle(title)
-    }
-
-    /** Returns an [[IO]] containing the code for maximizing the state of the JFrame wrapped by this instance. */
-    def maximizedAndExtendedStateSet(): IO[Unit] =
-      IO {
-        component.setExtendedState(component.getExtendedState | Frame.MAXIMIZED_BOTH)
-      }
   }
 
 }
