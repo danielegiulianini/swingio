163f2cdddeaf6b4336219852edf4936460a1cda6
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 16:18:29 +0100

    add scaladoc to JLabel monadic pimped methods

--- src/main/scala/swingio/scala/components/JLabelImplicits.scala
+++ src/main/scala/swingio/scala/components/JLabelImplicits.scala
@@ -7,10 +7,12 @@ object JLabelImplicits {
 
   implicit class JLabelIO(jLabel: JLabel) {
 
-    
+    /** Returns an [[IO]] containing the description of a [[JLabel#setText]]
+     * method invocation on the [[JLabel]] wrapped in this instance. */
     def setText(text: String): IO[Unit] = IO {jLabel.setText(text)}
 
-
+    /** Returns an [[IO]] containing the description of a [[JLabel#getText]]
+     * method invocation on the [[JLabel]] wrapped in this instance. */
     def getText: IO[String] = IO {jLabel.getText}
   }
 
