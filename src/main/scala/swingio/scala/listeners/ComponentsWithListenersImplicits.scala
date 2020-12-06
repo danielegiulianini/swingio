998b34a51b122afc6b7658e7c102eaae6e9662ae
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 11:21:15 +0100

    enable monadic changeListener specification for JSlider

--- src/main/scala/swingio/scala/listeners/ComponentsWithListenersImplicits.scala
+++ src/main/scala/swingio/scala/listeners/ComponentsWithListenersImplicits.scala
@@ -1,11 +1,16 @@
 package swingio.scala.listeners
 
-import javax.swing.JButton
-import swingio.scala.MonadicActionListener
+import javax.swing.{JButton, JSlider}
+import swingio.scala.{MonadicActionListener, MonadicChangeListener}
 
 trait ComponentsWithListenersImplicits {
   implicit class JButtonIO (button : JButton){
     def addMonadicActionListener(l : MonadicActionListener): Unit = button.addActionListener(l(_).unsafeRunSync())
   }
 
+  implicit class JSliderIO (button : JSlider) {
+    def addMonadicChangeListener(l: MonadicChangeListener): Unit = button.addChangeListener(l(_).unsafeRunSync())
+
+    def removeMonadicChangeListener(l: MonadicChangeListener): Unit = button.removeChangeListener(l(_).unsafeRunSync())
+  }
 }
