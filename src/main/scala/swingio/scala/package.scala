8b5ed2bc5408641ce536ee6576240604eac50ec8
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 17:00:50 +0100

    enable implicit conversion to IO monad to reuse constructors and document this change in scaladoc

--- src/main/scala/swingio/scala/package.scala
+++ src/main/scala/swingio/scala/package.scala
@@ -1,5 +1,7 @@
 package swingio
 
+import java.awt.Component
+
 import cats.effect.IO
 
 package object scala extends listeners.ListenersAliases with listeners.ListenersImplicits
@@ -10,7 +12,12 @@ package object scala extends listeners.ListenersAliases with listeners.Listeners
    * composition inside a for-comprehension construct with the [[IO]] semantics, that is: sequential computation
    * blocks described by an IO and composed by flatmap and map results in sequential side-effect execution when run
    * by [[cats.effect.IO#unsafeRunSync]].
+   * This conversion allows to automatically convert any member of
+   * [[java.awt.Component]] to IO, actually getting it ready to be used in for-comprehesion, and to reuse the
+   * constructors provided by the original library without need to rewrite each of them and manually encapsulates
+   * their results into IO monads.
+   * 
    * @see [[swingio.scala.examples.ExampleOfUse]] for example of how to compose IOs, and
    * [[IO]] for a detailed description of the IO data type.*/
-  //implicit def fromGenericTypeToIO[T](fa : T) : IO[T] = IO{ fa }
+  implicit def fromGenericTypeToIO[T<:Component](fa : T) : IO[T] = IO{ fa }
 }
