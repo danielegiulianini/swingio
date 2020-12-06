e60a8190e2b02673f1d5e7545064bde88a9cde46
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 16:05:28 +0100

    add scaladoc to JFrame monadic methods

--- src/main/scala/swingio/scala/components/JPanelImplicits.scala
+++ src/main/scala/swingio/scala/components/JPanelImplicits.scala
@@ -5,13 +5,15 @@ import javax.swing.JPanel
 import javax.swing.plaf.PanelUI
 
 object JPanelImplicits {
-
+  
   implicit class JPanelIO (jPanel: JPanel) {
 
-
+    /** Returns an [[IO]] containing the description of a [[JPanel#setUI]]
+     * method invocation on this [[JPanel]]. */
     def uiSet(ui:PanelUI): IO[Unit] = IO {	jPanel.setUI(ui)}
 
-    
+    /** Returns an [[IO]] containing the description of a [[JPanel#getUI]]
+     * method invocation on this [[JPanel]]. */
     def uiGot(): IO[PanelUI] = IO {jPanel.getUI}
   }
 
