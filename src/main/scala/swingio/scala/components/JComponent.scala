4b1cda552201ff96998a6374192d876bc20c6ee2
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 15:46:54 +0100

    add scaladoc to JComponentIO implicit class methods

--- src/main/scala/swingio/scala/components/JComponent.scala
+++ src/main/scala/swingio/scala/components/JComponent.scala
@@ -9,13 +9,16 @@ import javax.swing.border.Border
 object JComponent {
   implicit class JComponentIO(jComponent: JComponent) {
 
-
+    /** Returns an [[IO]] containing the description of a [[JComponent#setMinimumSize]]
+     * method invocation on this JComponent.*/
     def minimumSizeSet(dimension: Dimension): IO[Unit] = IO {jComponent.setMinimumSize(dimension)}
 
-
+    /** Returns an [[IO]] containing the description of a [[JComponent#setMaximumSize]]
+     * method invocation on this JComponent.*/
     def maximumSizeSet(dimension: Dimension): IO[Unit] = IO{ jComponent.setMaximumSize(dimension)}
 
-    
+    /** Returns an [[IO]] containing the description of a [[JComponent#setBorder]]
+     * method invocation on this JComponent.*/
     def borderSet(border: Border): IO[Unit] = IO {jComponent.setBorder(border)}
   }
 }
