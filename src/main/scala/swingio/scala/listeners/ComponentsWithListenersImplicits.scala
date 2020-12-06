8388d516f70405b901b6fd84cb9976ac3ea656b7
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 11:33:34 +0100

    add removeMonadicActionListener to JButton

--- src/main/scala/swingio/scala/listeners/ComponentsWithListenersImplicits.scala
+++ src/main/scala/swingio/scala/listeners/ComponentsWithListenersImplicits.scala
@@ -5,12 +5,16 @@ import swingio.scala.{MonadicActionListener, MonadicChangeListener}
 
 trait ComponentsWithListenersImplicits {
   implicit class JButtonIO (button : JButton){
+
     def addMonadicActionListener(l : MonadicActionListener): Unit = button.addActionListener(l(_).unsafeRunSync())
+
+    def removeMonadicActionListener(l: MonadicActionListener): Unit = button.removeActionListener(l(_).unsafeRunSync())
   }
 
-  implicit class JSliderIO (button : JSlider) {
-    def addMonadicChangeListener(l: MonadicChangeListener): Unit = button.addChangeListener(l(_).unsafeRunSync())
+  implicit class JSliderIO (slider : JSlider) {
 
-    def removeMonadicChangeListener(l: MonadicChangeListener): Unit = button.removeChangeListener(l(_).unsafeRunSync())
+    def addMonadicChangeListener(l: MonadicChangeListener): Unit = slider.addChangeListener(l(_).unsafeRunSync())
+
+    def removeMonadicChangeListener(l: MonadicChangeListener): Unit = slider.removeChangeListener(l(_).unsafeRunSync())
   }
 }
