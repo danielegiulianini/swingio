63ce6a6b6ec8e46e92ab628828f2e66e15c0ee5e
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 15:46:14 +0100

    add methods with IO return type for JComponent

--- src/main/scala/swingio/scala/components/JComponent.scala
+++ src/main/scala/swingio/scala/components/JComponent.scala
@@ -1,7 +1,21 @@
 package swingio.scala.components
 
-class JComponent {
-implicit class JComponentIO(jComponent: JComponent) {
-  
-}
+import java.awt.Dimension
+
+import cats.effect.IO
+import javax.swing.JComponent
+import javax.swing.border.Border
+
+object JComponent {
+  implicit class JComponentIO(jComponent: JComponent) {
+
+
+    def minimumSizeSet(dimension: Dimension): IO[Unit] = IO {jComponent.setMinimumSize(dimension)}
+
+
+    def maximumSizeSet(dimension: Dimension): IO[Unit] = IO{ jComponent.setMaximumSize(dimension)}
+
+    
+    def borderSet(border: Border): IO[Unit] = IO {jComponent.setBorder(border)}
+  }
 }
