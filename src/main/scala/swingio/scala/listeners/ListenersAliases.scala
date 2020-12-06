f00afb2356de59db0742a7327dce32b3e40eba09
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 11:32:00 +0100

    add scaladoc to MonadicActionListener

--- src/main/scala/swingio/scala/listeners/ListenersAliases.scala
+++ src/main/scala/swingio/scala/listeners/ListenersAliases.scala
@@ -6,10 +6,12 @@ import cats.effect.IO
 import javax.swing.event.ChangeEvent
 
 trait ListenersAliases {
+  /** Type alias for a monadic action listener, i.e. a listener whose behaviour upon [[ActionEvent]] is
+   * described by an IO monad (possibly result of a composition of IO monads).
+   * It is the monadic alternative to [[java.awt.event.ActionListener]] and it is used by
+   * [[swingio.scala.listeners.ComponentsWithListenersImplicits.JButtonIO#addMonadicActionListener]]*/
   type MonadicActionListener = ActionEvent => IO[Unit]
 
-  /** Type alias for a monadic change listener, i.e. a listener whose behaviour upon [[ChangeEvent]] is
-   * described by an IO monad (possibly result of a composition of IO monads).
-   * It is the monadic alternative to [[javax.swing.event.ChangeListener]]. */
+  
   type MonadicChangeListener = ChangeEvent => IO[Unit]
 }
