2c6c3354c7af8226cd122c523ab7c36f2b1853e9
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 11:15:25 +0100

    add ChangeListener monadic version

--- src/main/scala/swingio/scala/listeners/ListenersAliases.scala
+++ src/main/scala/swingio/scala/listeners/ListenersAliases.scala
@@ -3,7 +3,13 @@ package swingio.scala.listeners
 import java.awt.event.ActionEvent
 
 import cats.effect.IO
+import javax.swing.event.ChangeEvent
 
 trait ListenersAliases {
   type MonadicActionListener = ActionEvent => IO[Unit]
+
+  /** Type alias for a monadic change listener, i.e. a listener whose behaviour upon [[ChangeEvent]] is
+   * described by an IO monad (possibly result of a composition of IO monads).
+   * It is the alternative to [[javax.swing.event.ChangeListener]]. */
+  type MonadicChangeListener = ChangeEvent => IO[Unit]
 }
--- src/main/scala/swingio/scala/listeners/ListenersImplicits.scala
+++ src/main/scala/swingio/scala/listeners/ListenersImplicits.scala
@@ -1,9 +1,16 @@
 package swingio.scala.listeners
 
 import cats.effect.IO
+import javax.swing.event.ChangeEvent
 import swingio.scala._
 
 trait ListenersImplicits {
   implicit def unitToActionListener[T](f: => Unit): T => Unit = _ => f
   implicit def unitToMonadicActionListener(f: => IO[Unit]): MonadicActionListener = _ => f.unsafeRunSync()
+
+  /** Implicit utility for converting a by-name (or unevaluated) parameter expression provided by => syntax to
+   * [[MonadicChangeListener]] for enabling a more concise syntax at call-side when describing listeners that ignores
+   * the [[ChangeEvent]] triggering.*/
+  implicit def unitToMonadicChangeListener(f: => IO[Unit]): MonadicChangeListener = _ => f
+
 }
