449c2837003920302f34b07bf6738b98965f25fb
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 15:59:36 +0100

    add methods with IO return type for JFrame

--- src/main/scala/swingio/scala/components/JFrameImplicits.scala
+++ src/main/scala/swingio/scala/components/JFrameImplicits.scala
@@ -12,9 +12,55 @@ object JFrameImplicits {
 
     /** Returns an [[IO]] containing the description of a [[JFrameImplicits#getContentPane]]
      * method invocation. */
-    def contentPane(): IO[Container] = IO { jFrame.getContentPane() }
+    def getContentPane(): IO[Container] = IO { jFrame.getContentPane() }
 
+    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setResizable]]
+     * method invocation. */
+    def setResizable(resizable: Boolean): IO[Unit] = IO {
+      jFrame.setResizable(resizable)
+    }
 
+    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setVisible]]
+     * method invocation. */
+    def setVisible(b: Boolean): IO[Unit] = IO {
+      jFrame.setVisible(b)
+    }
+
+    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#pack]]
+     * method invocation. */
+    def pack(): IO[Unit] = IO {
+      jFrame.pack()
+    }
+
+    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setSize]]
+     * method invocation. */
+    def setSize(width: Int, height: Int): IO[Unit] = IO {
+      jFrame.setSize(width, height)
+    }
+
+    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setLocationRelativeTo]]
+     * method invocation. */
+    def setLocationRelativeTo(c: Component): IO[Unit] = IO {
+      jFrame.setLocationRelativeTo(c)
+    }
+
+    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setDefaultCloseOperation]]
+     * method invocation. */
+    def setDefaultCloseOperation(operation: Int): IO[Unit] = IO {
+      jFrame.setDefaultCloseOperation(operation)
+    }
+
+    /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setTitle]]
+     * method invocation. */
+    def setTitle(title: String): IO[Unit] = IO {
+      jFrame.setTitle(title)
+    }
+
+    /** Returns an [[IO]] containing the code for maximizing the state of the JFrame wrapped by this instance. */
+    def setExtendedState(): IO[Unit] =
+      IO {
+        jFrame.setExtendedState(jFrame.getExtendedState | Frame.MAXIMIZED_BOTH)
+      }
   }
 
 }
