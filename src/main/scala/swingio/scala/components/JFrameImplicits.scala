81f493d7c0c2b9a0001a4f14dbedc7f83f60266d
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-06 16:02:16 +0100

    tweak code formatting and scaladoc for JFrame

--- src/main/scala/swingio/scala/components/JFrameImplicits.scala
+++ src/main/scala/swingio/scala/components/JFrameImplicits.scala
@@ -11,54 +11,39 @@ object JFrameImplicits {
   implicit class JFrameIO(jFrame: JFrame) {
 
     /** Returns an [[IO]] containing the description of a [[JFrameImplicits#getContentPane]]
-     * method invocation. */
+     * method invocation on this JFrame. */
     def getContentPane(): IO[Container] = IO { jFrame.getContentPane() }
 
     /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setResizable]]
-     * method invocation. */
-    def setResizable(resizable: Boolean): IO[Unit] = IO {
-      jFrame.setResizable(resizable)
-    }
+     * method invocation on this JFrame. */
+    def setResizable(resizable: Boolean): IO[Unit] = IO { jFrame.setResizable(resizable) }
 
     /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setVisible]]
-     * method invocation. */
-    def setVisible(b: Boolean): IO[Unit] = IO {
-      jFrame.setVisible(b)
-    }
+     * method invocation on this JFrame. */
+    def setVisible(b: Boolean): IO[Unit] = IO { jFrame.setVisible(b) }
 
     /** Returns an [[IO]] containing the description of a [[JFrameImplicits#pack]]
-     * method invocation. */
-    def pack(): IO[Unit] = IO {
-      jFrame.pack()
-    }
+     * method invocation on this JFrame. */
+    def pack(): IO[Unit] = IO { jFrame.pack() }
 
     /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setSize]]
-     * method invocation. */
-    def setSize(width: Int, height: Int): IO[Unit] = IO {
-      jFrame.setSize(width, height)
-    }
+     * method invocation on this JFrame. */
+    def setSize(width: Int, height: Int): IO[Unit] = IO { jFrame.setSize(width, height) }
 
     /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setLocationRelativeTo]]
-     * method invocation. */
-    def setLocationRelativeTo(c: Component): IO[Unit] = IO {
-      jFrame.setLocationRelativeTo(c)
-    }
+     * method invocation on this JFrame. */
+    def setLocationRelativeTo(c: Component): IO[Unit] = IO { jFrame.setLocationRelativeTo(c) }
 
     /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setDefaultCloseOperation]]
-     * method invocation. */
-    def setDefaultCloseOperation(operation: Int): IO[Unit] = IO {
-      jFrame.setDefaultCloseOperation(operation)
-    }
+     * method invocation on this JFrame. */
+    def setDefaultCloseOperation(operation: Int): IO[Unit] = IO { jFrame.setDefaultCloseOperation(operation) }
 
     /** Returns an [[IO]] containing the description of a [[JFrameImplicits#setTitle]]
-     * method invocation. */
-    def setTitle(title: String): IO[Unit] = IO {
-      jFrame.setTitle(title)
-    }
+     * method invocation on this JFrame. */
+    def setTitle(title: String): IO[Unit] = IO { jFrame.setTitle(title) }
 
-    /** Returns an [[IO]] containing the code for maximizing the state of the JFrame wrapped by this instance. */
-    def setExtendedState(): IO[Unit] =
-      IO {
+    /** Returns an [[IO]] containing the code for maximizing the state of this JFrame. */
+    def setExtendedState(): IO[Unit] =       IO {
         jFrame.setExtendedState(jFrame.getExtendedState | Frame.MAXIMIZED_BOTH)
       }
   }
