1e476760a0def7b7839c8210ff110e3d6307c172
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-07 12:01:30 +0100

    update scaldoc for monadicInvokeAndWait and monadicInvokeLater

--- src/main/scala/swingio/scala/concurrency/ConcurrencyUtilities.scala
+++ src/main/scala/swingio/scala/concurrency/ConcurrencyUtilities.scala
@@ -9,10 +9,26 @@ trait ConcurrencyUtilities {
       SwingUtilities.invokeAndWait(() => monadicRunnable.unsafeRunSync())*/
 
   /**
+   * Returns the [[IO]] containing the code for asynchronously executing the computation described inside the
+   * given IO parameter on the AWT event dispatching thread, in a thread-safe manner.
+   * This method should be used whenever a thread other than EDT needs to asynchronously update the GUI.
+   * Unlike [[monadicInvokeAndWait()]], upon calling "unsafeRunSync" on the IO returned, the control flow
+   * will immediately return from the [[SwingUtilities#invokeLater]] call wrapped in the IO returned and
+   * the GUI-related code will be executed after all pending AWT events have been processed.
+   * The actual execution of the [[IO]] return should not happen on the EDT thread without a proper
+   * thread or context-shifting mechanism.
+   *
+   * @param byNameMonadicRunnable the [[IO]] containing the GUI-related logic to be executed.
+   * @return an IO wrapping the [[SwingUtilities#invokeLater]] thread-safe call.
+   */
+  def monadicInvokeLater(byNameMonadicRunnable: =>IO[Unit]): IO[Unit] =
+    IO{ SwingUtilities.invokeLater(() => byNameMonadicRunnable.unsafeRunSync()) }
+
+  /**
    * Returns the [[IO]] containing the code for synchronously executing the computation described inside the
    * computation parameter on the AWT event dispatching thread, in a thread-safe manner.
    * This method should be used whenever a thread other than EDT needs to update the GUI.
-   * Unlike [[invokingLater]], upon calling "unsafeRunSync" on the IO returned, the control flow
+   * Unlike [[monadicInvokeLater]], upon calling "unsafeRunSync" on the IO returned, the control flow
    * will block until all pending AWT events have been processed and only then the
    * [[SwingUtilities#invokeLater]] wrapped call will return.
    * If searching for an asynchronous execution of [[IO]]'s content, must use [[monadicInvokeLater]] instead.
@@ -22,10 +38,6 @@ trait ConcurrencyUtilities {
    * @param byNameMonadicRunnable the [[IO]] containing the GUI-related logic to be executed.
    * @return an IO wrapping the [[SwingUtilities#invokeAndWait]] thread-safe call.
    */
-  def monadicInvokeLater(byNameMonadicRunnable: =>IO[Unit]): IO[Unit] =
-    IO{ SwingUtilities.invokeLater(() => byNameMonadicRunnable.unsafeRunSync()) }
-
-  
   def monadicInvokeAndWait(byNameMonadicRunnable: =>IO[Unit]): IO[Unit] =
     IO{ SwingUtilities.invokeAndWait(() => byNameMonadicRunnable.unsafeRunSync()) }
 
