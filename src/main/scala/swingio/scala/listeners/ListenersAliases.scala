f3cab738e2961a92b212d4d5700546d0a5662289
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-07 12:06:12 +0100

    tweak ListenersAliases doc

--- src/main/scala/swingio/scala/listeners/ListenersAliases.scala
+++ src/main/scala/swingio/scala/listeners/ListenersAliases.scala
@@ -10,12 +10,12 @@ trait ListenersAliases {
   /** Type alias for a monadic action listener, i.e. a listener whose behaviour upon [[ActionEvent]] is
    * described by an IO monad (possibly result of a composition of IO monads).
    * It is the monadic alternative to [[java.awt.event.ActionListener]] and it is used by
-   * [[swingio.scala.listeners.ComponentsWithListenersImplicits.JButtonIO#addMonadicActionListener]]*/
+   * [[swingio.scala.components.JButtonImplicits.JButtonIO#addMonadicActionListener]]*/
   type MonadicActionListener = ActionEvent => IO[Unit]
 
   /** Type alias for a monadic change listener, i.e. a listener whose behaviour upon [[ChangeEvent]] is
    * described by an IO monad (possibly result of a composition of IO monads).
    * It is the monadic alternative to [[javax.swing.event.ChangeListener]] and it is used by
-   * [[swingio.scala.listeners.ComponentsWithListenersImplicits.JSliderIO#addMonadicChangeListener]]*/
+   * [[swingio.scala.components.JSliderImplicits.JSliderIO#addMonadicChangeListener]]*/
   type MonadicChangeListener = ChangeEvent => IO[Unit]
 }
