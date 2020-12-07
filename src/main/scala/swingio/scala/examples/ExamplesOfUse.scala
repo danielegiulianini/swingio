504edf63ce132a657441b831d77ec6b77d2b0891
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-07 14:05:09 +0100

    add example showing monadic vs procedural listener specification

--- src/main/scala/swingio/scala/examples/ExamplesOfUse.scala
+++ src/main/scala/swingio/scala/examples/ExamplesOfUse.scala
@@ -62,3 +62,47 @@ object SimpleExampleWithTraditionalSwing extends App {
   frame.setVisible(true)
 }
 
+
+object ExampleWithMonadicVsProceduralListeners extends App {
+
+  val frameBuilt = for {
+    frame <- new JFrame()
+    _ <- frame.titleSet("Basic GUI with listeners")
+    _ <- frame.resizableSet(true)
+    _ <- frame.defaultCloseOperationSet(WindowConstants.EXIT_ON_CLOSE)
+  } yield frame
+
+  val panelBuilt = for {
+    panel <- new JPanel()
+    slider <- new JSlider()
+    label <- new JLabel("value: ?")
+    button <- new JButton("Click to display positive value.")
+    _ <- monadicInvokeAndWait(for {
+      _ <- panel.layoutSet(new BorderLayout())
+      _ <- panel.added(slider, BorderLayout.CENTER)
+      _ <- panel.added(label, BorderLayout.NORTH)
+      //monadic listener description:
+      _ <- button.monadicActionListenerAdded(for {
+        currentValue <- slider.valueGot
+        _ <- if (currentValue > 0) label.textSet("value: ".+(currentValue))
+        else IO.unit
+      } yield ())
+      //procedural listener:
+      _ <- button.actionListenerAdded(System.out.println("button pressed"))
+      _ <- panel.added(slider, BorderLayout.CENTER)
+      _ <- panel.added(label, BorderLayout.NORTH)
+      _ <- panel.added(button, BorderLayout.SOUTH)
+    } yield ())
+  } yield panel
+
+  val program = for {
+    frame <- frameBuilt
+    panel <- panelBuilt
+    cp <- frame.contentPane
+    _ <- cp.added(panel)
+    _ <- frame.visibleSet(true)
+  } yield ()
+
+  program unsafeRunSync
+}
+
