2694035d79e07f9aa9db638699cb55592b69fa34
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2020-12-07 15:56:40 +0100

    doc imports for JLabel

--- src/main/scala/swingio/scala/components/JLabelImplicits.scala
+++ src/main/scala/swingio/scala/components/JLabelImplicits.scala
@@ -3,6 +3,17 @@ package swingio.scala.components
 import cats.effect.IO
 import javax.swing.JLabel
 
+/** Contains implicit class that enriches [[JLabel]] with additional functionalities by leveraging what is
+ * referred to as "Pimp My Library Pattern".
+ * Use this imports:
+ * {{{
+ * import swingio.scala._
+ * import swingio.scala.components.JLabelImplicits._
+ * }}}
+ * to enable methods returning [[IO]] for combining methods invocation inside for-comprehension and
+ * providing a description of GUI-related logic.
+ *
+ * @see [[IO]] for details on IO monad and how to run it. */
 object JLabelImplicits {
 
   implicit class JLabelIO(jLabel: JLabel) {
